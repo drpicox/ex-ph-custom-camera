7b5f0e84bcb70133999105e43473bf812c283dcf
David Rodenas <david.rodenas@gmail.com>
2014-08-27 16:05:50 +0200

    crop captured image to a size of 800x400

--- platforms/ios/CustomCamera/Classes/CustomCameraViewController.m
+++ platforms/ios/CustomCamera/Classes/CustomCameraViewController.m
@@ -76,39 +76,20 @@
     CGFloat height = imageSize.height;
     CGFloat targetWidth = targetSize.width;
     CGFloat targetHeight = targetSize.height;
-    CGFloat scaleFactor = 0.0;
-    CGFloat scaledWidth = targetWidth;
-    CGFloat scaledHeight = targetHeight;
     CGPoint thumbnailPoint = CGPointMake(0.0, 0.0);
     
-    if (CGSizeEqualToSize(imageSize, targetSize) == NO) {
-        CGFloat widthFactor = targetWidth / width;
-        CGFloat heightFactor = targetHeight / height;
-        
-        if (widthFactor > heightFactor) {
-            scaleFactor = widthFactor; // scale to fit height
-        } else {
-            scaleFactor = heightFactor; // scale to fit width
-        }
-        scaledWidth = width * scaleFactor;
-        scaledHeight = height * scaleFactor;
-        
-        // center the image
-        if (widthFactor > heightFactor) {
-            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
-        } else if (widthFactor < heightFactor) {
-            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
-        }
-    }
+    thumbnailPoint.y = (targetHeight - height) * 0.5;
+    thumbnailPoint.x = (targetWidth - width) * 0.5;
     
     UIGraphicsBeginImageContext(targetSize); // this will crop
     
     CGRect thumbnailRect = CGRectZero;
     thumbnailRect.origin = thumbnailPoint;
-    thumbnailRect.size.width = scaledWidth;
-    thumbnailRect.size.height = scaledHeight;
+    thumbnailRect.size.width = width;
+    thumbnailRect.size.height = height;
     
     [sourceImage drawInRect:thumbnailRect];
+    
     
     newImage = UIGraphicsGetImageFromCurrentImageContext();
     if (newImage == nil) {
@@ -178,6 +159,11 @@
     
     // Correct orientation
     image = [self imageCorrectedForCaptureOrientation:image];
+    
+    // Crop and resize
+    CGSize targetSize = CGSizeMake(800, 400);
+    image = [self imageByScalingAndCroppingForSize:image toSize:targetSize];
+    
     
 	// Get a file path to save the JPEG
 	//NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
