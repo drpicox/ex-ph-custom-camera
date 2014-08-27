d2b3d7f975a8da371b58d28de114c845520f176c
David Rodenas <david.rodenas@gmail.com>
2014-08-27 18:15:50 +0200

    tuned image capture, countdown starts with the button

--- platforms/ios/CustomCamera/Classes/CustomCameraViewController.m
+++ platforms/ios/CustomCamera/Classes/CustomCameraViewController.m
@@ -22,8 +22,7 @@
 		// Configure the UIImagePickerController instance
 		self.picker.sourceType = UIImagePickerControllerSourceTypeCamera;
 		self.picker.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
-        //cameraPicker.cameraDevice = (UIImagePickerControllerCameraDevice)[cameraDirection intValue];
-		self.picker.cameraDevice = UIImagePickerControllerCameraDeviceFront;//UIImagePickerControllerCameraDeviceRear;
+		self.picker.cameraDevice = UIImagePickerControllerCameraDeviceFront; //..Rear;
 		self.picker.showsCameraControls = NO;
         
 		// Make us the delegate for the UIImagePickerController
@@ -34,21 +33,15 @@
 		self.view.frame = screenFrame;
 		self.picker.view.frame = screenFrame;
         
+        
 		// Set this VC's view as the overlay view for the UIImagePickerController
 		self.picker.cameraOverlayView = self.view;
 
-		// Take a picture automatically 3 seconds after        
-        [self performSelector:@selector(setCountdown:) withObject:@"2" afterDelay:1.5f];
-        [self performSelector:@selector(setCountdown:) withObject:@"1" afterDelay:2.5f];
-        [self performSelector:@selector(setCountdown:) withObject:@"0" afterDelay:3.5f];
-        
         // Load image mask
         self.frameImage.image = [UIImage imageNamed:@"www/CameraMask.png"];        
-        
-        // Take the picture
-        [self performSelector:@selector(takePictureTo) withObject:nil afterDelay:3.5f];
-        
-        self.countdownLabel.text = @"3";
+
+        // Do not show countdown... for now
+        self.countdownLabel.hidden = true;
 	}
 	return self;
 }
@@ -64,7 +57,18 @@
 // Action method.  This is like an event callback in JavaScript.
 -(IBAction) takePhotoButtonPressed:(id)sender forEvent:(UIEvent*)event {
 	// Call the takePicture method on the UIImagePickerController to capture the image.
-	[self.picker takePicture];
+	//[self.picker takePicture];
+    
+    // Take a picture automatically 3 seconds after
+    [self performSelector:@selector(setCountdown:) withObject:@"2" afterDelay:1.5f];
+    [self performSelector:@selector(setCountdown:) withObject:@"1" afterDelay:2.5f];
+    [self performSelector:@selector(setCountdown:) withObject:@"0" afterDelay:3.5f];
+    
+    // Take the picture
+    [self performSelector:@selector(takePictureTo) withObject:nil afterDelay:3.5f];
+
+    self.countdownLabel.text = @"3";
+    self.countdownLabel.hidden = false;
 }
 
 - (UIImage*)imageByScalingAndCroppingForSize:(UIImage*)anImage toSize:(CGSize)targetSize
@@ -76,17 +80,30 @@
     CGFloat height = imageSize.height;
     CGFloat targetWidth = targetSize.width;
     CGFloat targetHeight = targetSize.height;
+    CGFloat scaleFactor = 1;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
     CGPoint thumbnailPoint = CGPointMake(0.0, 0.0);
     
-    thumbnailPoint.y = (targetHeight - height) * 0.5;
-    thumbnailPoint.x = (targetWidth - width) * 0.5;
+    CGFloat widthFactor = targetWidth / width;
+    CGFloat heightFactor = targetHeight / height;
+    if (widthFactor > heightFactor) {
+        scaleFactor = widthFactor;
+    } else {
+        scaleFactor = heightFactor;
+    }
+    
+    scaledWidth = width * scaleFactor;
+    scaledHeight = height * scaleFactor;
+    thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+    thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
     
     UIGraphicsBeginImageContext(targetSize); // this will crop
     
     CGRect thumbnailRect = CGRectZero;
     thumbnailRect.origin = thumbnailPoint;
-    thumbnailRect.size.width = width;
-    thumbnailRect.size.height = height;
+    thumbnailRect.size.width = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
     
     [sourceImage drawInRect:thumbnailRect];
     
@@ -151,6 +168,14 @@
     return newImage;
 }
 
+// older api calls newer didFinishPickingMediaWithInfo
+- (void)imagePickerController:(UIImagePickerController*)picker didFinishPickingImage:(UIImage*)image editingInfo:(NSDictionary*)editingInfo
+{
+    NSDictionary* imageInfo = [NSDictionary dictionaryWithObject:image forKey:UIImagePickerControllerOriginalImage];
+    
+    [self imagePickerController:picker didFinishPickingMediaWithInfo:imageInfo];
+}
+
 // Delegate method.  UIImagePickerController will call this method as soon as the image captured above is ready to be processed.  This is also like an event callback in JavaScript.
 -(void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
@@ -161,15 +186,8 @@
     image = [self imageCorrectedForCaptureOrientation:image];
     
     // Crop and resize
-    CGSize targetSize = CGSizeMake(800, 400);
+    CGSize targetSize = CGSizeMake(1200, 500);
     image = [self imageByScalingAndCroppingForSize:image toSize:targetSize];
-    
-    
-	// Get a file path to save the JPEG
-	//NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-	//NSString* documentsDirectory = [paths objectAtIndex:0];
-	//NSString* filename = @"test.jpg";
-	//NSString* imagePath = [documentsDirectory stringByAppendingPathComponent:filename];
     
 	// Get the image data (blocking; around 1 second)
 	NSData* imageData = UIImageJPEGRepresentation(image, 0.5);
