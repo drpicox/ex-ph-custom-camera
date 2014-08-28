d27a7b460eb4e615c17d5eac5e087f239b35846b
David Rodenas <david.rodenas@gmail.com>
2014-08-28 12:33:16 +0200

    image binarization

--- platforms/ios/CustomCamera/Classes/CustomCameraViewController.m
+++ platforms/ios/CustomCamera/Classes/CustomCameraViewController.m
@@ -193,6 +193,9 @@
     CGSize targetSize = CGSizeMake(1200, 500);
     image = [self imageByScalingAndCroppingForSize:image toSize:targetSize];
     
+    // Change image representation
+    image = [self applyThreshold:image];
+    
 	// Get the image data (blocking; around 1 second)
 	NSData* imageData = UIImagePNGRepresentation(image); //UIImageJPEGRepresentation(image, 0.5);
     
@@ -203,5 +206,179 @@
 	// Tell the plugin class that we're finished processing the image
 	[self.plugin capturedImageWithPath:imagePath];
 }
+
+
+
+// https://github.com/OmidH/Filtrr/blob/master/FiltrrApp/FiltrrApp/Filtrr/UIImage%2BFiltrr.m
+
+- (UIImage*) applyThreshold:(UIImage*)image {
+    
+    int i = 0;
+    
+    CGContextRef cgctx = [self createARGBBitmapContextFromImage:image.CGImage];
+    size_t width = CGImageGetWidth(image.CGImage);
+    size_t height = CGImageGetHeight(image.CGImage);
+    
+    
+    
+    
+    
+    unsigned char* data = CGBitmapContextGetData (cgctx);
+    if (data != NULL) {
+        int max = width * height * 4;
+
+        // accummulate frequencies form two boxes
+        unsigned int frequencies[256];
+        for (i = 0; i < 256; i++) {
+            frequencies[i] = 0;
+        }
+        /* with external control point
+        [self accumulateFrequencies:frequencies from:data width:width height:height startX:575 startY:225];
+        [self accumulateFrequencies:frequencies from:data width:width height:height startX:575 startY:375];
+        [self accumulateFrequencies:frequencies from:data width:width height:height startX:225 startY:375];
+        [self accumulateFrequencies:frequencies from:data width:width height:height startX:925 startY:375];
+         */
+        // Four "points" of the MRZ
+        [self accumulateFrequencies:frequencies from:data width:width height:height startX:316 startY:375];
+        [self accumulateFrequencies:frequencies from:data width:width height:height startX:478 startY:375];
+        [self accumulateFrequencies:frequencies from:data width:width height:height startX:641 startY:375];
+        [self accumulateFrequencies:frequencies from:data width:width height:height startX:803 startY:375];
+        
+        int threshold, accum = 0; // max accum === 10000 (50*50 * 2)
+        for (threshold = 0; threshold < 256 && accum < 1800; threshold++) {
+            accum += frequencies[threshold];
+        }
+        if (accum > 2100) {
+            threshold--;
+        }
+        
+        for (i = 0; i < max; i+=4) {
+            
+            unsigned char red,green,blue;
+            
+            red = data[i+1];
+            green = data[i+2];
+            blue = data[i+3];
+            
+            int sum = (red + green + blue)/3;
+            if (sum > threshold) {
+                red = green = blue = 255;
+            } else {
+                red = green = blue = 0;
+            }
+            
+            data[i+1]=red;
+            data[i+2]=green;
+            data[i+3]=blue;
+        }
+    }
+    
+    UIImage *img = [self createImageFromContext:cgctx WithSize:CGSizeMake(width, height)];
+    
+    if (data) { free(data); }
+    
+    return img;
+}
+
+- (void) accumulateFrequencies:(unsigned int*)frequencies from:(unsigned char*)data width:(size_t)width height:(size_t)height startX:(int)startX startY:(int)startY {
+    
+    int x,y,sum;
+    
+    for (y = startY; y < startY + 50; y+=1) {
+        for (x = startX; x < startX + 50; x++) {
+            int offset = ((y*width)+x)*4;
+            sum = 0; // no alpha
+            sum += data[offset + 1];
+            sum += data[offset + 2];
+            sum += data[offset + 3];
+            frequencies[sum/3]++;
+        }
+    }
+}
+
+- (CGContextRef) createARGBBitmapContextFromImage:(CGImageRef) imageRef {
+    CGContextRef    context = NULL;
+	CGColorSpaceRef colorSpace;
+	void *          bitmapData;
+	int             bitmapByteCount;
+	int             bitmapBytesPerRow;
+    
+	// Get image width, height. We'll use the entire image.
+	size_t pixelsWide = CGImageGetWidth(imageRef);
+	size_t pixelsHigh = CGImageGetHeight(imageRef);
+    
+	// Declare the number of bytes per row. Each pixel in the bitmap in this
+	// example is represented by 4 bytes; 8 bits each of red, green, blue, and
+	// alpha.
+	bitmapBytesPerRow   = (pixelsWide * 4);
+	bitmapByteCount     = (bitmapBytesPerRow * pixelsHigh);
+    
+	// Use the generic RGB color space.
+	colorSpace = CGColorSpaceCreateDeviceRGB();
+	if (colorSpace == NULL)
+	{
+		fprintf(stderr, "Error allocating color space\n");
+		return NULL;
+	}
+    
+	// Allocate memory for image data. This is the destination in memory
+	// where any drawing to the bitmap context will be rendered.
+	bitmapData = malloc( bitmapByteCount );
+	if (bitmapData == NULL)
+	{
+		fprintf (stderr, "Memory not allocated!");
+		CGColorSpaceRelease( colorSpace );
+		return NULL;
+	}
+    
+	// Create the bitmap context. We want pre-multiplied ARGB, 8-bits
+	// per component. Regardless of what the source image format is
+	// (CMYK, Grayscale, and so on) it will be converted over to the format
+	// specified here by CGBitmapContextCreate.
+	context = CGBitmapContextCreate (bitmapData,
+									 pixelsWide,
+									 pixelsHigh,
+									 8,      // bits per component
+									 bitmapBytesPerRow,
+									 colorSpace,
+									 kCGImageAlphaPremultipliedFirst);
+	if (context == NULL)
+	{
+		free (bitmapData);
+		fprintf (stderr, "Context not created!");
+	}
+    
+    CGRect rect = {{0,0},{pixelsWide, pixelsHigh}};
+    //
+    //        // Draw the image to the bitmap context. Once we draw, the memory
+    //        // allocated for the context for rendering will then contain the
+    //        // raw image data in the specified color space.
+    CGContextDrawImage(context, rect, imageRef);
+    
+	// Make sure and release colorspace before returning
+	CGColorSpaceRelease( colorSpace );
+    
+	return context;
+}
+
+- (UIImage *) createImageFromContext:(CGContextRef) cgctx WithSize:(CGSize) size {
+    
+    if (cgctx == NULL)
+        // error creating context
+        return nil;
+    
+    CGContextScaleCTM(cgctx, 1, -1);
+    CGContextTranslateCTM(cgctx, 0, -size.height);
+    
+    CGImageRef   img = CGBitmapContextCreateImage(cgctx);
+    UIImage*     ui_img = [UIImage imageWithCGImage: img];
+    
+    CGImageRelease(img);
+    CGContextRelease(cgctx);
+    
+    return ui_img;
+}
+
+
 
 @end
